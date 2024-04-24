a797cff3ecdf68692d0d7cbbc10c88596128a19d
Hrithik Arun Kumar M <128972805+Hrithik0216@users.noreply.github.com>
2024-04-24 21:21:16 +0530

    Fixed Doc

--- src/Route/route.js
+++ src/Route/route.js
@@ -4,31 +4,35 @@ const { getAllDetails, calculatePrice } = require('../Controller/controller');
 
 /**
  * @swagger
- * /mealdeliveryapp/:
- *  get:
- *      description: Get all prices
- *      responses: 
- *          200: 
- *              description: Success
+ * /mealdeliveryapp/getAllDetails:
+ *   get:
+ *     summary: Get all prices
+ *     description: Get all prices from the database.
+ *     responses:
+ *       200:
+ *         description: Successful response with all prices.
+ *       500:
+ *         description: Internal server error.
  */
 router.get('/getAllDetails', getAllDetails);
 
 /**
  * @swagger
- * /mealdeliveryapp/calculate-cost:
+ * /mealdeliveryapp/calculateCost:
  *   post:
- *     summary: Calculation of delivery cost
- *     description: Calculate the total delivery cost based on various factors such as sector, org_id, total_distance, and item_type.
+ *     summary: Calculate delivery cost
+ *     description: Calculate the total delivery cost based on sector, org_id, total_distance, and item_type.
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
- *           
+ *           schema:
+ *             $ref: '#/components/schemas/DeliveryRequest'
  *     responses:
  *       200:
- *         description: Successful response with the total delivery cost
- *         content:
- *           application/json:
+ *         description: Successful response with the total delivery cost.
+ *       400:
+ *         description: Invalid request body.
  */
 router.post('/calculateCost', calculatePrice);
 
